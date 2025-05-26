1bfa3ffadadc7f7deedfabdf7efdfc365efb5c31
rafiqwe <rmlmanik502@gmail.com>
2025-05-26 22:28:30 +0600

    How to create action creator for easy to view and read

--- src/store.jsx
+++ src/store.jsx
@@ -33,14 +33,28 @@ console.log(store);
 // How to  Log in the initial state
 console.log("Initial state:", store.getState());
 
+// Create action creators for add task
+export const addTask = (task) => {
+  return { type: ADD_TASK, payload: task };
+};
+
+// Create action creators for delete task
+export const deleteTask = (index) => {
+  return { type: DELETE_TASK, payload: index };
+};
+
 // How to Dispatch an action to add task
 
-store.dispatch({ type: ADD_TASK, payload: "My name is Muhammad Rabbi" });
+// store.dispatch({ type: ADD_TASK, payload: "My name is Muhammad Rabbi" }); // or using action creator
+store.dispatch(addTask("My name is Muhammad Rabbi"));
 console.log("upDated state:", store.getState());
 
-store.dispatch({ type: ADD_TASK, payload: "I am a React Developer" });
+store.dispatch(addTask("I am a React Developer" ) );
 console.log("upDated state:", store.getState());
 
 // How to Dispatch an action to delete task
-store.dispatch({ type: DELETE_TASK, payload: 1 });
+// store.dispatch({ type: DELETE_TASK, payload: 1 }); // or using action creator
+store.dispatch(deleteTask(1));
 console.log("upDated state:", store.getState());
+
+
